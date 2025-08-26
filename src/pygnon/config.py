85fb2891d3e9ff2a2a2b1be77cf15f046ff914be
Arthur Pech <apechkechian@gmail.com>
2025-08-26 18:15:47 +0200

    add load_dotenv to config.py

--- src/pygnon/config.py
+++ src/pygnon/config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+env_path = os.path.join(os.path.dirname(__file__), '..', '..')
+load_dotenv(env_path)
 
 GBFS_BASE_URL = "https://gbfs.omega.fifteen.eu/gbfs/2.2/marseille/en"
 DATA_PATH = os.getenv("DATA_PATH")
