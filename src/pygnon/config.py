dfcdc1c73c68e402c7441c36575c0fba2eb8cd7a
Arthur Pech <apechkechian@gmail.com>
2025-08-29 16:31:55 +0200

    change database config

--- src/pygnon/config.py
+++ src/pygnon/config.py
@@ -4,14 +4,4 @@ env_path = os.path.join(os.path.dirname(__file__), '..', '..')
 
 GBFS_BASE_URL = 'https://gbfs.omega.fifteen.eu/gbfs/2.2/marseille/en'
 DATA_PATH = os.getenv('DATA_PATH')
-
-# Database configuration
-DB_CONFIG = {
-    'db_host' : os.getenv('DB_HOST'),
-    'db_port' : os.getenv('DB_PORT'),
-    'db_user' : os.getenv('DB_USER'),
-    'db_password' : os.getenv('DB_PASSWORD'),
-    'db_name' : os.getenv('DB_NAME'),
-    'db_user' : os.getenv('DB_APP_USER'),
-    'db_app_password' : os.getenv('DB_APP_PASSWORD')
-}
+DATABASE_URL = os.getenv('DATABASE_URL')
